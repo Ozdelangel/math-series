a88f8883cfe529ed6fd8e41fef461e78495bfd45
OsbornDelAngel <ozdelangel@gmail.com>
2021-12-25 10:17:02 -0800

    lab 2 still need some tests

--- math-series/math_series/series.py
+++ math-series/math_series/series.py
@@ -9,6 +9,10 @@ def fibonacci(n):
     then we are going to assign it to a new variable then flip it so it can keep going
 
     then return the answer.
+
+    I got a HUGE help from Code Review and also this youtube video 
+
+    https://www.youtube.com/watch?v=7Sv4NmvdHcw
     """
     if n == 1:
         return 1
@@ -17,7 +21,30 @@ def fibonacci(n):
     elif n > 2:
         return fibonacci(n-1) + fibonacci(n-2)
 
+def lucas(n):
+    """
+    This is the Lucas Function
+    Takes a number as input
+    It will tell you what the number in the lucas it is
+    Once again, code review helped me with this
+    """
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return lucas(n-1) + lucas(n-2)
 
-
-if __name__ == "__main__":
-    print(fibonacci(3))
+def sum_series(n, parameter1=0, parameter2=1):
+    """
+    this is the sum series
+    it will take three params with the last two being optional
+    the first parameter will determine which element in the series to print
+    the second will default to 0 and 1 and will come up up with the first two values for the series
+    Big help from code review
+    """
+    if parameter1 == 0:
+        return fibonacci(n)
+    elif parameter2 == 2:
+        return lucas(n)
+   
--- math-series/tests/test_math_series.py
+++ math-series/tests/test_math_series.py
@@ -1,5 +1,44 @@
 from math_series import __version__
+from math_series.series import fibonacci, lucas
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_fibonacci_one():
+    actual = fibonacci(1)
+    expected = 1
+    assert actual == expected
+
+def test_fibonacci_two():
+    actual = fibonacci(2)
+    expected = 1
+    assert actual == expected
+
+def test_fibonacci_eight():
+    actual = fibonacci(8)
+    expected = 21
+    assert actual == expected
+
+def test_fibonacci_is_not_eight():
+    actual = fibonacci(8)
+    expected = 22
+    assert actual != expected
+
+def test_lucas_zero():
+    actual = lucas(0)
+    expected = 2
+    assert actual == expected
+def test_lucas_one():
+    actual = lucas(1)
+    expected = 1
+    assert actual == expected
+def test_lucas_two():
+    actual = lucas(2)
+    expected = 3
+    assert actual == expected
+def test_lucas_eight():
+    actual = lucas(8)
+    expected = 47
+    assert actual == expected
+
